1cf98000f77e1c69897e1991352075321a520d03
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-16 00:29:18 +0600

    JS Core Concept

---
+++ var-let-const.js
@@ -0,0 +1,10 @@
+//----------- let
+let age = 20;
+age = 21;
+//----------- const
+const countryName = 'Bangladesh';
+countryName = 'India' // doesn't work
+console.log(countryName); //error
+//------------ var
+var name = 'Mehedi Hasan';
+var name = 'Hasan Mehedi'
