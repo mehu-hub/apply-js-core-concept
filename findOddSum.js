6a8221a29898ee770a1d31a85bae01c08df1aa81
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-18 01:56:50 +0600

    Get Odd Number From Array

--- findOddSum.js
+++ findOddSum.js
@@ -1,6 +1,6 @@
-function getSum (numbers){
+function getSum(numbers) {
     let sum = 0;
-    for(let i = 0; i < numbers.length; i++ ){
+    for (let i = 0; i < numbers.length; i++) {
         const index = i;
         const element = numbers[index]
         sum = sum + element;
@@ -9,5 +9,20 @@ function getSum (numbers){
     return sum;
 }
 
+//------------------------------------------
+
 const myNumbers = [12, 65, 45, 78, 99, 20]
 getSum(myNumbers);
+
+function getOddNumFromArray(numbers) {
+    for (let i = 0; i < numbers.length; i++) {
+        const index = i;
+        const element = numbers[index];
+        if (element % 2 !== 0) {
+            console.log(element);
+        }
+    }
+}
+
+const numArray = [23, 25, 20, 52, 95, 78, 49, 47, 1, 13];
+getOddNumFromArray(numArray);
